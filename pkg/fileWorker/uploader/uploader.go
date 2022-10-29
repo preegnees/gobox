959dd77d4d59ed4f88a2d0609a5aa8b8cda00060
preegnees <mrraziapov13@gmail.com>
2022-10-29 17:58:28 +0300

    Пакет для загрузки данных файлов в директории на сервер [привести в порядок]

--- pkg/fileWorker/uploader/uploader.go
+++ pkg/fileWorker/uploader/uploader.go
@@ -11,21 +11,29 @@ import (
 
 	"github.com/sirupsen/logrus"
 
+	protocol "github.com/preegnees/gobox/pkg/fileWorker/protocol"
 	utils "github.com/preegnees/gobox/pkg/fileWorker/utils"
 )
 
-const UPLOAD_CODE = 100
+// IUploader. ...
+type IUploader interface {
+	Upload() error
+}
+
+type IClient interface {
+	Send(protocol.Info) error
+}
 
 type Uploader struct {
 	Ctx      context.Context
 	Log      *logrus.Logger
 	Dir      string
 	PrintErr func(string, string, error) error
-	Client   utils.IClient
+	Client   IClient
 }
 
 func (u *Uploader) Upload() error {
-	
+
 	f, err := os.Stat(u.Dir)
 	if err != nil {
 		return err
@@ -34,15 +42,13 @@ func (u *Uploader) Upload() error {
 	if !f.IsDir() {
 		return errors.New(fmt.Sprintf("path: %s is not dir", u.Dir))
 	}
-	
-	printErrDefault := func(description string, arg string, err error) error {
-		e := fmt.Errorf("[watcher] (err: %w) %s: %s", err, description, arg)
-		u.Log.Error(e)
-		return e
-	}
 
 	if u.PrintErr == nil {
-		u.PrintErr = printErrDefault
+		u.PrintErr = func(description string, arg string, err error) error {
+			e := fmt.Errorf("[watcher] (err: %w) %s: %s", err, description, arg)
+			u.Log.Error(e)
+			return e
+		}
 	}
 
 	if err := u.upload(u.Dir); err != nil {
@@ -52,45 +58,65 @@ func (u *Uploader) Upload() error {
 }
 
 func (u *Uploader) upload(path string) error {
-	u.Log.Debug(fmt.Sprintf("[watcher] upload(): %s", path))
+
+	u.Log.Debug(fmt.Sprintf("[uploader] upload(): %s", path))
 
 	files, err := ioutil.ReadDir(path)
 	if err != nil {
-		return u.PrintErr("upload() err", "", err)
+		return u.PrintErr("[uploader] upload() err", "", err)
 	}
 
-	for _, v := range files {
+	for _, file := range files {
 		select {
 		case <-u.Ctx.Done():
 			return nil
 		default:
-			curPath := filepath.Join(path, v.Name())
+			curPath := filepath.Join(path, file.Name())
+
+			pass := false
+			for _, val := range utils.IGNORE_STRS {
+				if strings.Contains(curPath, val) {
+					pass = true
+				}
+			}
+
+			if pass {
+				u.Log.Debug(fmt.Sprintf("[uploader] path include IGNORE_STRS, path: %s", curPath))
+				continue
+			}
 
 			modTime, err := utils.GetModTime(u.PrintErr, curPath)
 			if err != nil {
 				return err
 			}
 
-			info := utils.Info{
-				Action:  UPLOAD_CODE,
-				Path:    strings.ReplaceAll(curPath, u.Dir, ""),
-				ModTime: modTime,
+			hash, err := utils.GetHash(u.PrintErr, u.Log, curPath)
+			if err != nil {
+				return err
+			}
+
+			isFolder, err := utils.IsFolder(u.PrintErr, u.Log, curPath)
+			if err != nil {
+				return u.PrintErr("[uploader]", "", err)
+			}
+
+			info := protocol.Info{
+				Action:   protocol.UPLOAD_CODE,
+				Path:     curPath,
+				ModTime:  modTime,
+				Hash:     hash,
+				IsFolder: isFolder,
 			}
 
 			if err := u.Client.Send(info); err != nil {
-				return u.PrintErr("Method Send of Client failed", info.ToString(), err)
+				return u.PrintErr("[uploader] Method Send of Client failed", info.ToString(), err)
 			}
 
-			u.Log.Debug("Sent Info: ", info.ToString())
-
-			isFolder, err := utils.IsFolder(u.PrintErr, *u.Log, curPath)
-			if err != nil {
-				return u.PrintErr("", "", err)
-			}
+			u.Log.Debug(fmt.Sprintf("[uploader] Sent Info: %s", info.ToString()))
 
 			if isFolder {
 				if err := u.upload(curPath); err != nil {
-					return u.PrintErr("", "", err)
+					return u.PrintErr("[uploader]", "", err)
 				}
 			}
 		}
