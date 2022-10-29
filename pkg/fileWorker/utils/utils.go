6060716b70bdfa4a9f0fe41d78610b5c8a176627
preegnees <mrraziapov13@gmail.com>
2022-10-29 17:59:48 +0300

    пакет, который содержит общие функции, была добавлена функция взятия хеша для файла и папка [привести в порядок]

--- pkg/fileWorker/utils/utils.go
+++ pkg/fileWorker/utils/utils.go
@@ -1,14 +1,21 @@
 package utils
 
 import (
+	"crypto/sha256"
+	"encoding/hex"
 	"fmt"
+	"io"
 	"os"
+	"strconv"
 
 	"github.com/sirupsen/logrus"
-	"github.com/fsnotify/fsnotify"
 )
 
-func GetModTime(printErr func (string, string, error) error, path string) (int64, error) {
+var (
+	IGNORE_STRS = []string{"~", "__gobox__"}
+)
+
+func GetModTime(printErr func(string, string, error) error, path string) (int64, error) {
 
 	f, err := os.Stat(path)
 	if err != nil {
@@ -18,7 +25,7 @@ func GetModTime(printErr func (string, string, error) error, path string) (int64
 	return f.ModTime().UTC().UnixMicro(), nil
 }
 
-func IsFolder(printErr func (string, string, error) error, log logrus.Logger, path string) (bool, error) {
+func IsFolder(printErr func(string, string, error) error, log *logrus.Logger, path string) (bool, error) {
 
 	log.Debug(fmt.Sprintf("[watcher] isFolder(): %s", path))
 
@@ -30,23 +37,44 @@ func IsFolder(printErr func (string, string, error) error, log logrus.Logger, pa
 	return fileInfo.IsDir(), nil
 }
 
-type IClient interface {
-	Send(Info) error
+func GetHash(printErr func(string, string, error) error, log *logrus.Logger, fileName string) (string, error) {
+
+	f, err := os.Open(fileName)
+	if err != nil {
+		return "", printErr("GetHash() err open file", fileName, err)
+	}
+	defer f.Close()
+
+	isFolder, err := IsFolder(printErr, log, fileName) 
+	if err != nil {
+		return "", err
+	}
+
+	if isFolder {
+		h := sha256.New()
+		tm, err := GetModTime(printErr, fileName)
+		if err != nil {
+			return "", err
+		}
+		
+		_, err = h.Write([]byte(strconv.Itoa(int(tm))))
+		if err != nil {
+			return "", err
+		}
+
+		hash := hex.EncodeToString(h.Sum(nil))
+
+		log.Debug(fmt.Sprintf("GetHash() folder: %s, hash: %s", fileName, hash))
+		return hash, nil
+	}
+
+	h := sha256.New()
+	if _, err := io.Copy(h, f); err != nil {
+		return "", printErr("GetHash() err io copy file", fileName, err)
+	}
+
+	hash := hex.EncodeToString(h.Sum(nil))
+
+	log.Debug(fmt.Sprintf("GetHash() filename: %s, hash: %s", fileName, hash))
+	return hash, nil
 }
-
-var (
-	IGNORE_STRS = []string{"~", "__gobox__"}
-)
-
-// Info. Inforamtion about event
-type Info struct {
-	Action  fsnotify.Op
-	Path    string
-	ModTime int64
-}
-
-func (i *Info) ToString() string {
-	return fmt.Sprintf("Action: %v; Path: %s; ModTime: %d;", i.Action, i.Path, i.ModTime)
-}
-
-const PATH = "TestDir"
