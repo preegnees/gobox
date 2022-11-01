8dceb3bd76cc1bb1f8e872909d15a5f630f08a17
preegnees <mrraziapov13@gmail.com>
2022-11-02 01:48:57 +0300

    сделал отключение кейс ветки с помощью syncOnceDo

--- pkg/fileWorker/initializer/initializer.go
+++ pkg/fileWorker/initializer/initializer.go
@@ -10,7 +10,7 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-// TODO(там есть код 101, нужно вынести его в протокол или ютилс)
+//
 
 type IClient interface {
 	Send(p.Info) error
@@ -33,14 +33,14 @@ type ConfInitializer struct {
 }
 
 type initializer struct {
-	cnf ConfInitializer
+	cnf   ConfInitializer
 	infos map[string]p.Info
 }
 
 func New(cnf ConfInitializer) (initializer, error) {
 	return initializer{
 		infos: make(map[string]p.Info),
-		cnf: cnf,
+		cnf:   cnf,
 	}, nil
 }
 
@@ -73,25 +73,24 @@ func (i *initializer) Initize() error {
 		mx.Unlock()
 	}
 
+	var once sync.Once
+
 	for {
 		select {
 		case <-i.cnf.Ctx.Done():
 			return nil
 		case e, ok := <-eventChUploader:
 			if !ok {
-				return nil
-			}
-			if e.Action == 101 {
-				for _, info := range i.infos {
-					if err := i.cnf.Client.Send(info); err != nil {
+				once.Do(func() {
+					for _, info := range i.infos {
+						if err := i.cnf.Client.Send(info); err != nil {
+							panic(err)
+						}
+					}
+					if err := i.cnf.Client.Send(e); err != nil {
 						panic(err)
 					}
-				}
-				if err := i.cnf.Client.Send(e); err != nil {
-					panic(err)
-				}
-				close(eventChUploader)
-				eventChUploader = nil
+				})
 			}
 			add(e)
 		case e, ok := <-eventChWatcher:
