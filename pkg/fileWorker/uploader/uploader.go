0c0ae05f09b85ee416ba9431445cfd06d802c05f
preegnees <mrraziapov13@gmail.com>
2022-11-02 01:46:46 +0300

    добавил логгирование, ошибки, дополнительный метод интерфейса для получения канала, в New сделал валидация, вместо отсылания кода 101 -> закрытие канала, с помощью errorgroup сделал асинхронность

--- pkg/fileWorker/uploader/uploader.go
+++ pkg/fileWorker/uploader/uploader.go
@@ -10,172 +10,206 @@ import (
 	"strings"
 
 	"github.com/sirupsen/logrus"
+	"golang.org/x/sync/errgroup"
 
-	protocol "github.com/preegnees/gobox/pkg/fileWorker/protocol"
-	utils "github.com/preegnees/gobox/pkg/fileWorker/utils"
+	p "github.com/preegnees/gobox/pkg/fileWorker/protocol"
+	ut "github.com/preegnees/gobox/pkg/fileWorker/utils"
+)
+
+var (
+	ERROR__IOUTIL_READDIR_METHOD__ = errors.New("Err ioutil.ReadDir")
+	ERROR__OS_STAT_METHOD__ = errors.New("Err os.Stat cnf.Dir")
+	ERROR__IS_NOT_DIR__     = errors.New("is not dir")
 )
 
 var _ IUploader = (*Uploader)(nil)
 
-// IUploader. ...
+// IUploader. интерфейс для взаимодействия с пакетом
 type IUploader interface {
 	Upload() error
-	GetEventChan() chan protocol.Info
+	GetEventChan() chan p.Info
 }
 
-// ConfUploader. ...
+// ConfUploader. конфигурация для загрузчика
 type ConfUploader struct {
-	Ctx      context.Context
-	Log      *logrus.Logger
-	Dir      string
-	PrintErr func(desc string, arg string, err error) error
+	Ctx context.Context
+	Log *logrus.Logger
+	Dir string
 }
 
-// Uploader. ...
+func (c *ConfUploader) ToString() string {
+
+	return fmt.Sprintf(
+		"context: %v, levelLog: %s, dir: %s",
+		c.Ctx, c.Log.Level, c.Dir,
+	)
+}
+
+// Uploader. структура загрузчика
 type Uploader struct {
-	ctx      context.Context
-	cancel   context.CancelFunc
-	log      *logrus.Logger
-	dir      string
-	printErr func(string, string, error) error
-	EventCh  chan protocol.Info
+	ctx     context.Context
+	cancel  context.CancelFunc
+	log     *logrus.Logger
+	dir     string
+	eventCh chan p.Info
 }
 
-func New(cnf ConfUploader) *Uploader {
+func (u *Uploader) ToString() string {
+
+	return fmt.Sprintf(
+		"context: %v, levelLog: %s, dir: %s, eventCh: %v",
+		u.ctx, u.log.Level, u.dir, u.eventCh,
+	)
+}
+
+// New. создает новый загрзчик
+func New(cnf ConfUploader) (*Uploader, error) {
+
+	cnf.Log.Debug(fmt.Sprintf("[uploader.New()] struct cnf: %v;", cnf.ToString()))
+
+	f, err := os.Stat(cnf.Dir)
+	if err != nil {
+		cnf.Log.Error(fmt.Errorf("[uploader.New()] stat error, err: %w, path: %s;", err, cnf.Dir))
+		return nil, fmt.Errorf("%w (%s);", ERROR__OS_STAT_METHOD__, err.Error())
+	}
+
+	if !f.IsDir() {
+		cnf.Log.Error(fmt.Errorf("[uploader.New()] path: %s is not dir", cnf.Dir))
+		return nil, fmt.Errorf("%w;", ERROR__IS_NOT_DIR__)
+	}
 
 	ctx, cancel := context.WithCancel(cnf.Ctx)
 
-	var printE func(string, string, error) error
-	if cnf.PrintErr == nil {
-		printE = func(description string, arg string, err error) error {
-			e := fmt.Errorf("[uploader] (err: %w) %s: %s", err, description, arg)
-			cnf.Log.Error(e)
-			return e
-		}
-	} else {
-		printE = cnf.PrintErr
-	}
-
-	cnf.Log.Println("uploader creating")
+	cnf.Log.Debug("[uploader.New()] uploader creating;")
 
 	return &Uploader{
-		ctx:      ctx,
-		cancel:   cancel,
-		log:      cnf.Log,
-		dir:      cnf.Dir,
-		printErr: printE,
-		EventCh:  make(chan protocol.Info),
-	}
+		ctx:     ctx,
+		cancel:  cancel,
+		log:     cnf.Log,
+		dir:     cnf.Dir,
+		eventCh: make(chan p.Info),
+	}, nil
 }
 
 func (u *Uploader) Upload() error {
 
 	defer u.cancel()
 
-	f, err := os.Stat(u.dir)
-	if err != nil {
-		return err
-	}
-
-	if !f.IsDir() {
-		return errors.New(fmt.Sprintf("path: %s is not dir", u.dir))
-	}
-
-	if u.printErr == nil {
-		u.printErr = func(description string, arg string, err error) error {
-			e := fmt.Errorf("[watcher] (err: %w) %s: %s", err, description, arg)
-			u.log.Error(e)
-			return e
-		}
-	}
-
 	if err := u.upload(u.dir); err != nil {
 		return err
 	}
+
+	close(u.eventCh)
+	u.eventCh = nil
+
 	return nil
 }
 
 func (u *Uploader) upload(path string) error {
 
-	u.log.Debug(fmt.Sprintf("[uploader] upload(): %s", path))
+	u.log.Debug(fmt.Sprintf("[uploader.upload()] path: %s;", path))
 
 	files, err := ioutil.ReadDir(path)
 	if err != nil {
-		return u.printErr("[uploader] upload() err", "", err)
+		u.log.Error(fmt.Errorf("[uploader.onStart()] err: %w, path: %s;", err, path))
+		return fmt.Errorf("%w (%s);", ERROR__IOUTIL_READDIR_METHOD__, err.Error())
 	}
 
-	var mainErr error
+	var errr error
 
 	for _, file := range files {
 		select {
 		case <-u.ctx.Done():
+
+			u.log.Debug(fmt.Sprintf("[uploader.Watch()] context done;"))
 			break
 		default:
 			curPath := filepath.Join(path, file.Name())
 
+			u.log.Debug(fmt.Sprintf("[uploader.Watch()] current path: %s;", curPath))
+
 			pass := false
-			for _, val := range utils.IGNORE_STRS {
+			for _, val := range ut.IGNORE_STRS {
 				if strings.Contains(curPath, val) {
+					u.log.Debug(fmt.Sprintf("[uploader.Watch()] name: %s include substr: %s;", curPath, val))
 					pass = true
 				}
 			}
 
 			if pass {
-				u.log.Debug(fmt.Sprintf("[uploader] path include IGNORE_STRS, path: %s", curPath))
 				continue
 			}
 
-			modTime, err := utils.GetModTime(u.printErr, curPath)
-			if err != nil {
-				mainErr = err
+			var modTime int64 = 0
+			var hash string = ""
+			var err error = nil
+			var isFolder bool = false
+
+			g := new(errgroup.Group)
+			g.Go(
+				func() error {
+					modTime, err = ut.GetModTime(u.log, curPath)
+					if err != nil {
+						return err
+					}
+					return nil
+				},
+			)
+			g.Go(
+				func() error {
+					hash, err = ut.GetHash(u.log, curPath)
+					if err != nil {
+						return err
+					}
+					return nil
+				},
+			)
+			g.Go(
+				func() error {
+					isFolder, err = ut.IsFolder(u.log, curPath)
+					if err != nil {
+						return err
+					}
+					return nil
+				},
+			)
+			if err := g.Wait(); err != nil {
+				errr = err
 				break
 			}
 
-			hash, err := utils.GetHash(u.printErr, u.log, curPath)
-			if err != nil {
-				mainErr = err
-				break
-			}
-
-			isFolder, err := utils.IsFolder(u.printErr, u.log, curPath)
-			if err != nil {
-				mainErr = u.printErr("[uploader]", "", err)
-				break
-			}
-
-			info := protocol.Info{
-				Action:   protocol.UPLOAD_CODE,
+			info := p.Info{
+				Action:   p.UPLOAD_CODE,
 				Path:     curPath,
 				ModTime:  modTime,
 				Hash:     hash,
 				IsFolder: isFolder,
 			}
 
-			u.EventCh <- info
+			u.eventCh <- info
 
-			u.log.Debug(fmt.Sprintf("[uploader] Sent Info: %s", info.ToString()))
+			u.log.Debug(fmt.Sprintf("[uploader.Watch()] Sent Info: %s", info.ToString()))
 
 			if isFolder {
 				if err := u.upload(curPath); err != nil {
-					mainErr = u.printErr("[uploader]", "", err)
+					errr = err
 					break
 				}
 			}
 		}
 	}
 
-	u.EventCh <- protocol.Info{
-		Action: (101),
-	}
-
-	if mainErr != nil {
-		return mainErr
+	if errr != nil {
+		return errr
 	}
 
 	return nil
 }
 
-func (u *Uploader) GetEventChan() chan protocol.Info {
-	
-	return u.EventCh
+// GetEventChan. Получение канала для получения событий
+func (u *Uploader) GetEventChan() chan p.Info {
+
+	u.log.Debug(fmt.Sprintf("[uploader.GetEventChan()];"))
+
+	return u.eventCh
 }
