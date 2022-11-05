d568e9063e7765578c00e3e686ca12fcb43d4cb9
preegnees <mrraziapov13@gmail.com>
2022-11-06 00:03:30 +0300

    тесты для проверки пакета сохранения файла

---
+++ pkg/client/file/saver/saver_test.go
@@ -0,0 +1,90 @@
+package saver
+
+import (
+	"os"
+	"testing"
+)
+
+var TEST_FILE = "TEST_FILE.txt"
+
+func TestGetPath(t *testing.T) {
+	s := saver{}
+	mainPath := "hello\\world\\newFolder\\new.txt"
+	want := "hello\\world\\newFolder\\" + PREFFIX + "new.txt"
+	newPath := s.getPath(mainPath)
+	t.Log(newPath)
+	if newPath != want {
+		panic("newPath != want")
+	}
+
+	oldPath := s.getPath(newPath)
+	t.Log(oldPath)
+	if oldPath != mainPath {
+		panic("oldPath != mainPath")
+	}
+}
+
+func TestResizeFile(t *testing.T) {
+
+	f, err := os.Create(TEST_FILE)
+	if err != nil {
+		panic(err)
+	}
+	f.Close()
+
+	defer os.Remove(TEST_FILE)
+
+	s := saver{
+		storage: make(map[string]*os.File),
+	}
+
+	file, err := os.OpenFile(TEST_FILE, os.O_RDWR, 0777)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	s.storage[TEST_FILE] = file
+
+	startSize := int64(1024*50)
+	data := make([]byte, startSize)
+	file.Write(data)
+
+	stat, err := os.Stat(TEST_FILE)
+	if err != nil {
+		panic(err)
+	}
+
+	if startSize != stat.Size() {
+		panic("startSize != stat.Size()")
+	}
+
+	newSize1 := int64(1024*50 + 1024*100)
+	if err := s.resize(TEST_FILE, newSize1); err != nil {
+		panic(err)
+	}
+
+	stat, err = os.Stat(TEST_FILE)
+	if err != nil {
+		panic(err)
+	}
+
+	if stat.Size() != newSize1 {
+		panic("stat.Size() != newSize1")
+	}
+
+	newSize2 := int64(1024*50 - 1024*30)
+	if err := s.resize(TEST_FILE, newSize2); err != nil {
+		panic(err)
+	}
+
+	stat, err = os.Stat(TEST_FILE)
+	if err != nil {
+		panic(err)
+	}
+
+	t.Log(stat.Size())
+
+	if stat.Size() != newSize2 {
+		panic("stat.Size() != newSize1")
+	}
+}
